0371866e362ea3877b4efd23394a35d1de66631b
Matt L <matt@Matts-MacBook-Pro-2.local>
2017-08-06 21:41:55 -0700

    add back a validation

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,8 +2,7 @@ class Member < ApplicationRecord
 	has_many :member_groups
 	has_many :needs
 	belongs_to :user
-	# validates :sdcc_member_id, :email, :name, :phone, presence: true
-
+	validates :sdcc_member_id, :email, :name, :phone, presence: true
 	validates_uniqueness_of :sdcc_member_id 
 	# user_id: integer, sdcc_member_id: integer, name: string, phone: string, email: string, covered: boolean,
 
