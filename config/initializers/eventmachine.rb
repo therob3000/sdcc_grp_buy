18a8f00eba4fad48aabc71e0af426927ab72cfc2
Matt L <matt@Matts-MacBook-Pro-2.local>
2017-09-03 17:16:08 -0700

    add dev env tools

--- config/initializers/eventmachine.rb
+++ config/initializers/eventmachine.rb
@@ -1,2 +1,4 @@
-require 'thread'
-Thread.new { EventMachine.run } unless EventMachine.reactor_running? && EventMachine.reactor_thread.alive?
+if !Rails.env.development?
+	require 'thread'
+	Thread.new { EventMachine.run } unless EventMachine.reactor_running? && EventMachine.reactor_thread.alive?
+end
